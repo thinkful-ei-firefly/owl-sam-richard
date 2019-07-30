3be65258e36dd1c549e7f09855d96e144816fcc5
Sam Walker <jack.nasal@gmail.com>
2019-07-30 16:57:30 -0400

    added Stage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Participant from './Participant';
 import ChatLog from './ChatLog';
+import Stage from './Stage';
 import './App.css';
 
 function App({store:{participant, chatLog}}) {
@@ -11,21 +12,22 @@ function App({store:{participant, chatLog}}) {
       <Participant key='1' participant={participant} />
       </div>
       <div className='Chat'>
-      {chatLog.map((chat, id) => {
-        console.log('something here');
-        let result = ( <ChatLog
-          key={id} 
-          type={chat.type}
-          avatar={participant.find(item =>item.id===chat.participantId).avatar}
-          name={participant.find(item =>item.id===chat.participantId).name}
-          message={chat.message}
-          time={chat.time}
-          timestamp={chat.timestamp}
-      /> )
-      console.log(result);
-      return result; 
-      })}
+        {chatLog.map((chat, id) => {
+          console.log('something here');
+          let result = ( <ChatLog
+            key={id} 
+            type={chat.type}
+            avatar={participant.find(item =>item.id===chat.participantId).avatar}
+            name={participant.find(item =>item.id===chat.participantId).name}
+            message={chat.message}
+            time={chat.time}
+            timestamp={chat.timestamp}
+        /> )
+        console.log(result);
+        return result; 
+        })}
       </div>
+      <Stage participant={participant} />
     </div>
   );
 }
--- src/Stage.js
+++ src/Stage.js
@@ -1,1 +1,19 @@
 import React from 'react';
+
+function Stage(props) {
+  const onStage = props.participant.filter(item => item.onStage);
+  return (
+    <div class= 'stage-container'>
+      {onStage.map(person => {
+        return (
+          <div className='onStage' key={props.participant.id}>
+            <div>{person.name}</div>
+            <img src={person.avatar} alt='avatar' />
+          </div>
+        )
+      })}
+    </div>
+  );
+}
+
+export default Stage;
